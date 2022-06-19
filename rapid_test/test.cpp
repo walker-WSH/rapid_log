8b30febb965924ada9e0eb45a2be3642766990f9
wangshaohui <wang.shaohui@navercorp.com>
2022-06-19 16:39:03 +0800

    save

--- rapid_test/test.cpp
+++ rapid_test/test.cpp
@@ -1,15 +1,21 @@
 ﻿#include <rapid_log.h>
+#include <Windows.h>
 
 int main()
 {
 	init_log(L"G:\\test.txt");
 
-	LOG_INFO("This is for %s, num: %d", "wsh", 789);
-	LOG_INFO("This is for %1%, num: %2%", "test", 123);
-	LOG_INFO("This is for %s, num: %d", "demo", "test");
+	DWORD start = GetTickCount();
+	for (size_t i = 0; i < 2000; i++) {
+		LOG_INFO("This is for %s, num: %d", "wsh", 789);
+		LOG_INFO("This is for %1%, num: %2%", "test", 123);
+		LOG_INFO("This is for %s, num: %d", "demo", "test");
 
-	LOG_INFO("This is for %1%, num: %d", "test", 123);
-	LOG_INFO("This is for %1%, num: %2%", "test");
+		LOG_INFO("This is for %1%, num: %d", "test", 123);
+		LOG_INFO("This is for %1%, num: %2%", "test");
+	}
+	DWORD end = GetTickCount();
+	printf("take %u ms \n\n", end - start);
 
 	uninit_log();
 
